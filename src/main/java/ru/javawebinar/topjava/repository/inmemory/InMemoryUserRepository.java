ad4fdf535543e612226e834638db30321888abcc
DIAK ANNA <64887157+anndiak@users.noreply.github.com>
2021-02-28 12:37:41 +0200

    Last

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -7,39 +7,63 @@ import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
 
+    Map<Integer,User> usersRepository = new ConcurrentHashMap<>();
+    AtomicInteger counter = new AtomicInteger(0);
+
+    public static final int USER_ID = 1;
+    public static final int Admin_ID = 2;
+
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+        return usersRepository.remove(id) != null;
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return user;
+        if (user.isNew()) {
+            user.setId(counter.incrementAndGet());
+            usersRepository.put(user.getId(), user);
+            return user;
+        }
+        // handle case: update, but not present in storage
+        return usersRepository.computeIfPresent(user.getId(), (id, oldUser) -> user);//atomic operation
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return usersRepository.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+
+        return usersRepository.values().stream()
+                .sorted(Comparator.comparing(User::getName)
+                        .thenComparing(User::getEmail))
+                .collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return null;
+        return getAll().stream()
+                .filter(u -> u.equals(email))
+                .findFirst()
+                .orElse(null);
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,16 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.web.user.AdminRestController;
+
+import java.util.Arrays;
 
 public class MealRestController {
     private MealService service;
+
 
 }
